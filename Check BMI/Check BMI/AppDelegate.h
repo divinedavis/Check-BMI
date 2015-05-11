d4ba6cd6383ef8ee73abf8335e17f22c7e81e7a8
divinedavis <divinejdavis@gmail.com>
2015-05-11 00:40:03 -0400

    First commit of the new Check BMI app The first one somehow got destroyed when i updated the Xcode with the beta

---
+++ Check BMI/Check BMI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Check BMI
+//
+//  Created by Divine Davis on 5/11/15.
+//  Copyright (c) 2015 Portfolio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Check BMI/Check BMI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Check BMI
+//
+//  Created by Divine Davis on 5/11/15.
+//  Copyright (c) 2015 Portfolio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
